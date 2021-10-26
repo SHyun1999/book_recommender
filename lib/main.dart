94daa2eabce872c70fde9f94bf7d45027a39bfac
SHyun1999 <86813210+SHyun1999@users.noreply.github.com>
2021-10-26 15:14:06 -0500

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
-import 'package:rflutter_alert/rflutter_alert.dart';
+
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
+import 'bookrecs.dart';
+import 'Recommend.dart';
 
 void main() => runApp(new MaterialApp(home: new Main()));
 
@@ -10,31 +11,7 @@ class Main extends StatelessWidget {
   String author = '';
   String title = '';
 
-  Future<List> RecommendBooks(var body) async {
-
-    var client = new http.Client();
-    var uri = Uri.parse("https://api-book-rec-last.herokuapp.com/RecommendBooks");
-
-
-    Map<String, String> headers = {"Content-type": "application/json"};
-    String jsonString = json.encode(body);
-
-
-    try {
-      var resp = await client.post(uri, headers: headers, body: jsonString);
-
-      if (resp.statusCode == 200) {
-
-        var result = await json.decode(resp.body);
-        return result;
-
-      }
-    } catch (e) {
-      print("EXCEPTION OCCURRED: $e");
-      return [];
-    }
-    return [];
-  }
+  Recommend r = Recommend();
 
   @override
   Widget build(BuildContext context) {
@@ -58,9 +35,9 @@ class Main extends StatelessWidget {
                 FlatButton(
                   color: Colors.blue,
                   onPressed: () async {
-                    var body =[title];
+                    var body = [title];
 
-                    var res = await RecommendBooks(body);
+                    var res = await r.RecommendBooks(body);
 
                     Navigator.push(
                       context,
@@ -75,31 +52,6 @@ class Main extends StatelessWidget {
             ),
           ),
         ),
-      ),
-    );
-  }
-}
-
-class BookRecs extends StatelessWidget {
-
-  const BookRecs({Key? key, required this.res}) : super(key: key);
-
-  final List<dynamic> res;
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Recommended books are:'),
-      ),
-
-      body: ListView.builder(
-        itemCount: res.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(res[index]),
-          );
-        },
       ),
     );
   }
